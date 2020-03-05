6d9b73f8aacec59788c9118d5fc9f60fe5d5d721
luhugerth <luisa.hugerth@scilifelab.se>
2020-03-05 15:28:53 +0100

    handles reads with pairing info on the header

--- shotgun_tools/make_roc_curve.py
+++ shotgun_tools/make_roc_curve.py
@@ -8,41 +8,63 @@ import csv
 import argparse
 import gzip
 from Bio import SeqIO
+import re
 
 
 def parse_args():
-    """Scales nearly with input file size, be careful"""
+    """Scales linearly with input file size, be careful"""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--table", "-t", dest="class_tab", 
-            help="Unoise mapping to human UC output ")
+            help="Unoise UC or SAM table mapping to human")
+    parser.add_argument('--unoise', dest='unoise', action='store_true',
+            help="mapping table is in Unoise UC format")
+    parser.add_argument('--sam', dest='unoise', action='store_false',
+            help="mapping table is in SAM format (default)")
+    parser.set_defaults(unoise=False)
     parser.add_argument("fastqs", nargs="+", 
-            help="fastq files to quantify human %")
+            help="fastq files to quantify human and non-human reads")
     
-    if len(argv) < 2:
+    if len(argv) < 3:
         parser.print_help()
         exit()
         
     return parser.parse_args()
 
 
-def sort_reads(class_tab):
+def sort_reads(class_tab, unoise):
     human = set()
     nonhuman = set()
     with open(class_tab) as csvfile:
         reader = csv.reader(csvfile, delimiter="\t")
-        for row in reader:
-            if(row[0]=="H"):
-                human.add(row[8])
-            else:
-                nonhuman.add(row[8])
+        if(unoise == True):
+            print("Parsing Unoise file...")
+            for row in reader:
+                if(row[0]=="H"):
+                    human.add(row[8])
+                else:
+                    nonhuman.add(row[8])
+        else:
+            print("Parsing SAM file...")
+            for row in reader:
+                if(row[0][0]!="@"):
+                    if(row[5]=='*'):
+                        nonhuman.add(row[0])
+                    else:
+                        human.add(row[0])
     # Sanity check in case Usearch did something weird:                
+    print(len(human))
+    print(len(nonhuman))
     nonhuman = nonhuman.difference(human)
     return(human, nonhuman)
 
 
 def parse_fastq(infile, humanlist, buglist):
     with gzip.open(infile, "rt") as handle:
-        record_set = set(SeqIO.to_dict(SeqIO.parse(handle, "fastq")))
+        record_list = list(SeqIO.to_dict(SeqIO.parse(handle, "fastq")))
+        record_list = [record.replace('/1', '') for record in record_list]
+        record_list = [record.replace('/2', '') for record in record_list]
+        print(record_list)
+        record_set = set(record_list)
         #True positives: human reads NOT in record
         tpr = len(humanlist.difference(record_set))
         #False positives: bacterial reads NOT in record
@@ -62,6 +84,7 @@ def parse_fastqs(fastq_files, humanreads, bugreads):
     fnr = [None] * len(fastq_files)
     for i in range(0, len(fastq_files)):
         myfile = fastq_files[i]
+        print("Parsing", myfile)
         tp, fp, tn, fn = parse_fastq(myfile, humanreads, bugreads)
         tpr[i] = tp
         fpr[i] = fp
@@ -70,8 +93,8 @@ def parse_fastqs(fastq_files, humanreads, bugreads):
     return(tpr, fpr, tnr, fnr)
 
 
-def main(class_tab, fastq_files):
-    human, nonhuman = sort_reads(class_tab)
+def main(class_tab, unoise, fastq_files):
+    human, nonhuman = sort_reads(class_tab, unoise)
     TPR, FPR, TNR, FNR = parse_fastqs(fastq_files, human, nonhuman)
     print("File\t" + "\t".join(fastq_files))
     print("True positives\t" + "\t".join([str(elem) for elem in TPR]))
@@ -81,4 +104,5 @@ def main(class_tab, fastq_files):
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.class_tab, args.fastqs)
+    print(args)
+    main(args.class_tab, args.unoise, args.fastqs)
