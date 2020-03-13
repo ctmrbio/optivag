f84c1caabbbfa11e0e7a7f83bcbb736127451ff4
luhugerth <luisa.hugerth@scilifelab.se>
2020-03-13 12:28:24 +0100

    add functionality to create seqid2tax.id.map

---
+++ database/tools/map_seq_tax.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Uses the assembly summary and the list of downloaded genomes to create
+a taxonomy file compatible with Bracke2
+"""
+
+import argparse
+import csv
+import glob
+import os
+
+def read_taxa(taxa):
+    id_dict = dict()
+    with open(taxa) as csvfile:
+        reader = csv.reader(csvfile, delimiter="\t")
+        for row in reader:
+            id_dict[row[0]] = row[1]
+    return(id_dict)
+
+            
+def make_list(id_dict, genomes, outfile):   
+    with open(outfile, "w+") as o:
+        for filename in glob.glob(os.path.join(genomes, '*.fna')):
+            genome = os.path.basename(filename)
+            tax_id = id_dict[genome]
+            with open(filename, 'r') as csvfile:
+                reader = csv.reader(csvfile, delimiter=" ")
+                for row in reader:
+                    if row[0][0]==">":
+                        seq_id = row[0][1:]
+                        o.write(seq_id + "\t" + tax_id + "\n")
+
+
+
+def main(taxa, genomes, outfile):
+    id_dict = read_taxa(taxa)
+    make_list(id_dict, genomes, outfile)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Combines sequence headers with the appropriate taxonomy code')
+    parser.add_argument('-t', '--taxa', help='TSV file connecting each genome to its NCBI tax_id')
+    parser.add_argument('-g', '--genomes', help='folder containing each genome in the database')
+    parser.add_argument('-o', '--out', nargs = '?', default='seqid2taxid.map', help='path to outfile (default seqid2taxid.map)')
+    args = parser.parse_args()
+
+    main(args.taxa, args.genomes, args.out)
+
