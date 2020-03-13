5888e5cba86222643e9af22f5044e64d343ddad9
luhugerth <luisa.hugerth@scilifelab.se>
2020-03-13 09:13:29 +0100

    also produces a file_name to taxon_id mapping

--- database/tools/filter_found_genomes.py
+++ database/tools/filter_found_genomes.py
@@ -20,12 +20,14 @@ def parse_infile(infile):
         complete_genomes = defaultdict(lambda: defaultdict(str))
         chromosomes = defaultdict(lambda: defaultdict(str))
         scaffolds = defaultdict(lambda: defaultdict(str))
+        tax_dict = defaultdict(lambda: defaultdict(str))
         
         reader = csv.reader(csvfile, delimiter="\t")
         for row in reader:
             if(row[0][0] != "#"):
                 genome_type=row[4]
-                tax_id=row[7]
+                tax_id = row[5]
+                tax_name=row[7]
                 strain=row[8]
                 if "=" in strain:
                     strain=strain.split("=")[1]
@@ -33,16 +35,18 @@ def parse_infile(infile):
                     strain=row[9]
                 seq_type=row[11]
                 url=row[19]
-                taxa = tax_id.split(" ")
+                taxa = tax_name.split(" ")
                 
                 if(len(taxa)>2 and taxa[2] == "subsp."):
                     taxon = " ".join(taxa[0:4])
-                elif(len(taxa)>2 and taxa[2] == "genomosp."):
+                elif(len(taxa)>2 and taxa[1] == "genomosp."):
                     taxon = " ".join(taxa[0:3])
                 else:
                     taxon = " ".join(taxa[0:2])
                 eachspecies.add(taxon)
                 strain = " ".join([taxon, strain])
+
+                tax_dict[taxon][strain] = tax_id
 
                 if(genome_type == "reference genome"):
                     ref_genomes[taxon][strain] = url
@@ -54,20 +58,21 @@ def parse_infile(infile):
                     chromosomes[taxon][strain] = url
                 elif(seq_type == "Scaffold"):
                     scaffolds[taxon][strain] = url
-        return(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds)
+        return(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, tax_dict)
 
 
-def print_reference(outgenomes, outproteins, strain_name, url):
-    with open(outgenomes, "a+") as g, open(outproteins, "a+") as p:
+def print_reference(outgenomes, outproteins, outtaxa, strain_name, url, tax_id):
+    with open(outgenomes, "a+") as g, open(outproteins, "a+") as p, open(outtaxa, "a+") as t:
         parts = url.split("/")
         identifier = parts[-1]
         g.write("wget " + url + "/" + identifier + "_genomic.fna.gz\n")
         g.write("mv " +  identifier + "_genomic.fna.gz " + strain_name + "_genomic.fna.gz\n")
         p.write("wget " + url + "/" + identifier + "_protein.faa.gz\n")
         p.write("mv " +  identifier + "_protein.faa.gz " + strain_name + "_protein.faa.gz\n")
+        t.write(strain_name + "_genomic.fna\t" + tax_id + "\n")
 
 
-def select_genomes(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, maxgenomes, outgenomes, outproteins):
+def select_genomes(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, taxa_id, maxgenomes, outgenomes, outproteins, outtaxa):
     for species in eachspecies:
         genomes_found = 0
         if(species in ref_genomes):
@@ -75,38 +80,42 @@ def select_genomes(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffold
             for strain in strains:
                 if(genomes_found < maxgenomes):
                     genomes_found += 1
+                    tax_id = taxa_id[species][strain]
                     strain_name = strain.replace(" ", "_")
                     url = ref_genomes[species][strain]
-                    print_reference(outgenomes, outproteins, strain_name, url)
+                    print_reference(outgenomes, outproteins, outtaxa, strain_name, url, tax_id)
         if(species in repr_genomes and genomes_found < maxgenomes):
             strains = repr_genomes[species].keys()
             for strain in strains:
                 if(genomes_found < maxgenomes):
                     genomes_found += 1
+                    tax_id = taxa_id[species][strain]
                     strain_name = strain.replace(" ", "_")
                     url = repr_genomes[species][strain]
-                    print_reference(outgenomes, outproteins, strain_name, url)
+                    print_reference(outgenomes, outproteins, outtaxa, strain_name, url, tax_id)
         if(species in chromosomes and genomes_found < maxgenomes):
             strains = chromosomes[species].keys()
             for strain in strains:
                 if(genomes_found < maxgenomes):
                     genomes_found += 1
+                    tax_id = taxa_id[species][strain]
                     strain_name = strain.replace(" ", "_")
                     url = chromosomes[species][strain]
-                    print_reference(outgenomes, outproteins, strain_name, url)
+                    print_reference(outgenomes, outproteins, outtaxa, strain_name, url, tax_id)
         if(species in scaffolds and genomes_found == 0):
             strains = scaffolds[species].keys()
             for strain in strains: 
                 if(genomes_found < maxgenomes):
                     genomes_found += 1
+                    tax_id = taxa_id[species][strain]
                     strain_name = strain.replace(" ", "_")
                     url = scaffolds[species][strain]
-                    print_reference(outgenomes, outproteins, strain_name, url)
+                    print_reference(outgenomes, outproteins, outtaxa, strain_name, url, tax_id)
 
 
-def main(infile, outgenomes, outproteins, maxgenomes):
-    eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds = parse_infile(infile)
-    select_genomes(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, maxgenomes, outgenomes, outproteins)
+def main(infile, outgenomes, outproteins, outtaxa, maxgenomes):
+    eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, taxa_id = parse_infile(infile)
+    select_genomes(eachspecies, ref_genomes, repr_genomes, chromosomes, scaffolds, taxa_id, maxgenomes, outgenomes, outproteins, outtaxa)
 
 
 if __name__ == '__main__':
@@ -114,8 +123,9 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--infile', help='subset of assembly_summary with selected taxa')
     parser.add_argument('-g', '--outgenomes', help='sh file to be created for getting genome information')
     parser.add_argument('-p', '--outproteins', help='sh file to be created for getting protein information')
+    parser.add_argument('-t', '--outtaxa', help='tsv file to be created with the taxon id for each file')
     parser.add_argument('-n', '--maxgenomes', type=int, default=10, help="Maximum number of genomes per sub/species")
     args = parser.parse_args()
 
-    main(args.infile, args.outgenomes, args.outproteins, args.maxgenomes)
+    main(args.infile, args.outgenomes, args.outproteins, args.outtaxa, args.maxgenomes)
 
